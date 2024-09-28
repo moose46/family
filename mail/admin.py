671dd9e6b8f23f5b2b49dfa2345c8fd5f1f8060c
me <me@robertcurtiss.com>
2024-09-28 10:02:18 -0400

    Added City Model

--- mail/admin.py
+++ mail/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Mail
+
+from mail.models import Mail, Address
+
+
+@admin.register(Mail)
+class MailAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    pass
--- mail/models.py
+++ mail/models.py
@@ -1,8 +1,13 @@
 import string
+from datetime import date
+# from importlib.resources._common import _
+
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils.text import slugify
-from os import name
+
+
+# from django.utils.text import slugify
+# from os import name
 
 
 # Create your models here.
@@ -16,22 +21,21 @@ class Base(models.Model):
 
 
 class Person(Base):
-    name = models.CharField(max_length=32, default="", null=False, unique=True)
-    slug = models.TextField(blank=True)
+    name: str = models.CharField(max_length=32, default="", null=False, unique=True)
+    slug: str = models.TextField(blank=True)
 
-    def save(
-        self, force_insert=False, force_update=False, using=None, update_fields=None
-    ):
-        self.slug = slugify(f"{self.name}")
-        # print(self.slug)
-        if update_fields is not None and name in update_fields:
-            update_fields = {"slug"}.union(update_fields)
-        super().save(
-            force_insert=force_insert,
-            force_update=force_update,
-            using=using,
-            update_fields=update_fields,
-        )
+    # def save(
+    #     self, force_insert=False, force_update=False, using=None, update_fields=None )
+    #     self.slug = slugify(f"{self.name}")
+    #     # print(self.slug)
+    #     if update_fields is not None and name in update_fields:
+    #         update_fields = {"slug"}.union(update_fields)
+    #     super().save(
+    #         force_insert=force_insert,
+    #         force_update=force_update,
+    #         using=using,
+    #         update_fields=update_fields
+    #     )
 
     def __str__(self) -> str:
         return self.name
@@ -56,27 +60,39 @@ class State(Base):
         unique = "name"
         ordering = "name"
 
-from django.contrib.localflavor.us.models import USStateField
-from django.utils.translation import ugettext as _
-
-from django.contrib.localflavor.us.us_states import STATE_CHOICES
+class City(Base):
+    name = models.CharField(max_length=32, unique=True)
+    def __str__(self) -> str:
+        return self.name
 
 class Address(Base):
-    YOUR_STATE_CHOICES = list(STATE_CHOICES)
-    YOUR_STATE_CHOICES.insert(0, ('', '---------'))
+    address_1 = models.CharField("address", max_length=128)
+    address_2 = models.CharField("address cont'd", max_length=128, blank=True)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+    zip = models.CharField(max_length=32)
+    # YOUR_STATE_CHOICES = list(STATE_CHOICES)
+    # YOUR_STATE_CHOICES.insert(0, ('', '---------'))
 
-    address_1 = models.CharField(_("address"), max_length=128)
-    address_2 = models.CharField(_("address cont'd"), max_length=128, blank=True)
 
-    city = models.CharField(_("city"), max_length=64, default="Zanesville")
-    state = USStateField(_("state"), default="OH")
-    zip_code = models.CharField(_("zip code"), max_length=5, default="43701",widget=forms.Select(
-            choices=YOUR_STATE_CHOICES))
+    # city = models.CharField(_("city"), max_length=64, default="Zanesville")
+    # state = USStateField(_("state"), default="OH")
+    # zip_code = models.CharField(_("zip code"), max_length=5, default="43701",widget=forms.Select(
+    #         choices=YOUR_STATE_CHOICES))
+
+
+class MailFrom(Base):
+    name: Person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    address: Address = models.ForeignKey(Address, on_delete=models.CASCADE)
+
+
+class MailTo(Base):
+    name: Person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    address: Address = models.ForeignKey(Address, on_delete=models.CASCADE)
+
 
 class Mail(Base):
-    postmark = models.DateField()
-    mail_from = models.ForeignKey(Person, on_delete=models.CASCADE)
-    mail_to = models.ForeignKey(Person, on_delete=models.CASCADE)
+    postmark: date = models.DateField()
+    mail_to = models.ForeignKey(MailTo, on_delete=models.CASCADE)
+    mail_from = models.ForeignKey(MailFrom, on_delete=models.CASCADE)
     contents = models.TextField()
-    address_to = models.ForeignKey(Address, on_delete=models.CASCADE)
-    address_from = models.ForeignKey(Address, on_delete=models.CASCADE)
